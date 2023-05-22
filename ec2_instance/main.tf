e156a4908f1aa19824f20b3868616aab0df00b2a
VladyslavSkvortsovLab <129038403+VladyslavSkvortsovLab@users.noreply.github.com>
2023-05-22 20:23:59 +0300

    Update main.tf

--- ec2_instance/main.tf
+++ ec2_instance/main.tf
@@ -17,9 +17,6 @@ terraform {
 # Configure AWS provider and creds
 provider "aws" {
   region = "us-east-1"
-  shared_config_files = ["../config.txt"]
-shared_credentials_files = ["../credentials.txt"]
-profile = "default"
 }
 
 # Get the latest Amazon Linux ami id
